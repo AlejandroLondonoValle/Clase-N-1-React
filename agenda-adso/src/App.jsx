dd9f20583876e107a0f6baaa02bf7d54efe4e801
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-02-06 11:46:55 -0500

    Personalizacion del componente App

--- agenda-adso/src/App.jsx
+++ agenda-adso/src/App.jsx
@@ -1,45 +1,43 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
+import './App.css'
 
 export default function App() {
-  const fecha = new Date().toLocaleString();
+
   return (
+    <>
     <main>
+      <h1>¡Hola, Mi nombre es Luis Alejandro Londoño Valle!</h1>
       <hr />
-      <br />
-      <h1>Hola SENA {fecha}</h1>
-      <hr />
-      <br />
+      <p>Soy un joven de 19 años con conocimientos en C# y .NET, y un marcado interés en especializarme en desarrollo de software. Actualmente curso Tecnología en Análisis y Desarrollo de Software en el SENA y Ingeniería de Software en el Tecnológico de Antioquia, con el objetivo de adquirir continuamente nuevos conocimientos y experiencias que me permitan crecer en el ámbito tecnológico. Me caracterizo por ser una persona solidaria, trabajadora y responsable, comprometida con la búsqueda constante de oportunidades para mejorar mis habilidades y contribuir de manera significativa en proyectos innovadores.</p>
     </main>
+    
+    <footer className="footer">
+        <div className="footer-container">
+          <p id="copyright">© 2025 Luis Alejandro Londoño Valle · Construyendo backends impecables</p>
+
+          <div className="footer-links">
+            <a
+              href="https://github.com/AlejandroLondonoValle"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://co.linkedin.com/in/lu%C3%ADs-alejandro-londo%C3%B1o-valle"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+            <a href="mailto:londonovalleluisalejandro@gmail.com">
+              Email
+            </a>
+          </div>
+        </div>
+      </footer>
+
+    </>
+
   );
 }
+
